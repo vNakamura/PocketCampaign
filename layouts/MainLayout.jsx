8a7a4c2c45cf99aa21cad55a2c55f184f3eb4f69
Vinicius Nakamura <hello@vnakamura.com>
2016-02-21 23:19:35 -0300

    Adds NavBar background

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -1,7 +1,25 @@
 MainLayout = React.createClass({
+  getInitialState() {
+    return {
+      isStandalone: false
+    }
+  },
+
+  componentWillMount() {
+    this.setState({
+      isStandalone: Meteor.isClient? window.navigator.standalone : false
+    });
+  },
+
   render() {
+    const navBarClasses = ClassNames('navBar', {statusPadding: this.state.isStandalone});
     return (
-      <main className="fillHeight">{this.props.content}</main>
+      <main className="fillHeight">
+        <div className={navBarClasses}>
+          <h1>Testando</h1>
+        </div>
+        {this.props.content}
+      </main>
     );
   }
 });
