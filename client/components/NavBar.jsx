0d323995e858f68d1faf82e71d5e4fd2ce56c35c
Vinicius Nakamura <hello@vnakamura.com>
2016-03-20 15:09:47 -0300

    Extracts NavBar into a component

---
+++ client/components/NavBar.jsx
@@ -0,0 +1,24 @@
+NavBar = React.createClass({
+  getInitialState() {
+    return {
+      isStandalone: false
+    }
+  },
+
+  componentWillMount() {
+    this.setState({
+      isStandalone: Meteor.isClient? window.navigator.standalone : false
+    });
+  },
+
+  render() {
+    const navBarClasses = ClassNames('navBar', {withStatus: this.state.isStandalone});
+    return (
+      <div className={navBarClasses}>
+        <i className="sidebar link icon"></i>
+        <p className="title">Testando</p>
+        <i className="info circle link icon"></i>
+      </div>
+    )
+  }
+});
--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -1,32 +1,15 @@
 MainLayout = React.createClass({
-  getInitialState() {
-    return {
-      isStandalone: false
-    }
-  },
-
-  componentWillMount() {
-    this.setState({
-      isStandalone: Meteor.isClient? window.navigator.standalone : false
-    });
-  },
-
   render() {
-    const navBarClasses = ClassNames('navBar', {withStatus: this.state.isStandalone});
     return (
       <main className="wrapper">
-        <div className={navBarClasses}>
-          <i className="sidebar link icon"></i>
-          <p className="title">Testando</p>
-          <i className="info circle link icon"></i>
-        </div>
+        <NavBar/>
 
         <div className="layoutContent">
           {this.props.content}
         </div>
 
         <div className="messageInputBox ui menu">
-          <MessageInput />
+          <MessageInput/>
         </div>
       </main>
     );
