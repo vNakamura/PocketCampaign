4d2f9ade63ad7e43b11a72f7e2a17320d3c9fb88
Vinicius Nakamura <hello@vnakamura.com>
2016-08-03 00:28:41 -0300

    Fresh restart. No more Meteor. We will use Firebase

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './bin',
+    filename: 'app.bundle.js'
+  },
+   module: {
+     loaders: [
+       {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         loader: 'babel',
+       },
+       {
+         test: /\.styl$/,
+         loader: 'style!css!stylus'
+       }
+     ]
+   }
+}
