9ad82b33199f75f42423e31e420a781b2002aca5
Vinicius Nakamura <vnakamura@vnakamura.com>
2016-08-08 18:06:22 -0300

    Adds CSS Modules with Stylus and Autoprefixer

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { render } from 'react-dom';
 import React from 'react';
 import App from './components/App';
 
+require('./global.styl');
+
 render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
+autoprefixer = require('autoprefixer');
+
 module.exports = {
   entry: './src/main.js',
   output: {
     path: './bin',
     filename: 'app.bundle.js'
   },
-   module: {
-     loaders: [
-       {
-         test: /\.js$/,
-         exclude: /node_modules/,
-         loader: 'babel',
-       },
-       {
-         test: /\.styl$/,
-         loader: 'style!css!stylus'
-       }
-     ]
-   }
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.styl$/,
+        loader:
+          'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!stylus'
+      }
+    ]
+  },
+  postcss: function () {
+    return [autoprefixer];
+  }
 }
