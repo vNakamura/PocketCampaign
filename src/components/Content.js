9fb6518d86a694a58bc2770cf0ba30865af76b26
Vinicius Nakamura <vnakamura@vnakamura.com>
2016-08-10 15:56:13 -0300

    Creates Topbar component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,17 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 import style from './Content.styl';
 
+// Components
 import KitchenSink from './KitchenSink';
+import Topbar from './Topbar';
 
 class Content extends React.Component {
   render () {
     return(<div className={style.content}>
-      <KitchenSink/>
+      <Topbar showMenuButton={true} titleText="A really long title that would make the line break on a small screen"/>
+      <div className={style.scrollable}>
+        <KitchenSink/>
+      </div>
     </div>);
   }
 }
---
+++ src/components/Topbar.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+import style from './Topbar.styl';
+import { TiThMenu } from 'react-icons/lib/ti'
+
+class Topbar extends React.Component {
+  onMenuButtonClick (e) {
+    e.preventDefault();
+    console.log("Menu Button clicked");
+  }
+
+  renderMenuButton () {
+    return(
+      <a href="#" onClick={this.onMenuButtonClick.bind()} className={style.menuButton}><TiThMenu/></a>
+    );
+  }
+  render () {
+    return(
+      <div className={style.container}>
+        {this.props.showMenuButton? this.renderMenuButton() : ""}
+        <h5 className={style.title}>{this.props.titleText}</h5>
+      </div>
+    );
+  }
+}
+
+Topbar.propTypes = {
+  showMenuButton: PropTypes.bool,
+  titleText: PropTypes.string
+}
+
+Topbar.defaultProps = {
+  showMenuButton: false,
+  titleText: ''
+}
+
+export default Topbar;
