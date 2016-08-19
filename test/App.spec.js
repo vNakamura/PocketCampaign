0a57836a597486f875a27e875c6a74e776fef0ae
Vinicius Nakamura <hello@vnakamura.com>
2016-08-19 00:30:48 -0300

    Implements App component tests

---
+++ test/App.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import App from '../src/components/App';
+
+describe('<App />', function () {
+  it('should have a <Content /> component', function () {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Content')).to.have.length(1);
+  });
+
+  it('should have a <Sidebar /> component if state says so', function () {
+    const wrapper = shallow(<App />);
+    let visibility = wrapper.state('sidebarVisible') ? 1 : 0;
+    expect(wrapper.find('Sidebar')).to.have.length(visibility);
+    wrapper.setState({sidebarVisible: !visibility});
+    visibility = wrapper.state('sidebarVisible') ? 1 : 0;
+    expect(wrapper.find('Sidebar')).to.have.length(visibility);
+  });
+
+});
