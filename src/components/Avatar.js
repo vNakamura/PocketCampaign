f6d6edf81b3024153421457df7461defd69c7c09
Vinicius Nakamura <hello@vnakamura.com>
2016-08-21 20:43:51 -0300

    Adjusts Avatar component to work better with Message component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -14,18 +14,19 @@ function getImageSrc(props) {
 }
 
 const Avatar = (props) =>
-  <img
+  <div className={props.className}><img
     className={style.avatar}
     title={props.name}
     role="presentation"
     src={getImageSrc(props)}
     style={props.style}
-  />;
+  /></div>;
 
 Avatar.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string,
   style: PropTypes.object,
+  className: PropTypes.string,
 };
 
 export default Avatar;
