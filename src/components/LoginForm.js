62f744353f9cb48e7f8dedf08823d55144b0f42f
Vinicius Nakamura <vnakamura@vnakamura.com>
2017-03-14 18:01:24 -0300

    Show error message if login fail

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -8,15 +8,24 @@ export class LoginForm extends React.Component {
     this.state = {
       name: '',
       password: '',
+      error: null,
+      locked: false,
     };
   }
 
   handleSubmit = (e) => {
     if (e) e.preventDefault();
     const { email, password } = this.state;
-    this.props.firebase.login({
-      email,
-      password,
+    this.setState({ locked: true }, () => {
+      this.props.firebase.login({
+        email,
+        password,
+      }).catch((error) => {
+        this.setState({
+          error,
+          locked: false,
+        });
+      });
     });
   }
 
@@ -30,9 +39,20 @@ export class LoginForm extends React.Component {
     });
   }
 
+  renderError = () => {
+    const { error } = this.state;
+    if (error) {
+      return (
+        <p>Error: {error.message}</p>
+      );
+    }
+    return null;
+  }
+
   render() {
     return (
       <div>
+        {this.renderError()}
         <form
           onSubmit={this.handleSubmit}
         >
@@ -40,13 +60,19 @@ export class LoginForm extends React.Component {
             type="email"
             name="email"
             onChange={this.handleChange}
+            disabled={this.state.locked}
           />
           <input
             type="password"
             name="password"
             onChange={this.handleChange}
+            disabled={this.state.locked}
           />
-          <input type="submit" value="Sign In" />
+          <input
+            type="submit"
+            value="Sign In"
+            disabled={this.state.locked}
+          />
         </form>
       </div>
     );
