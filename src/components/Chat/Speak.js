18239369a6c8559e8bd6f3409e2e12b0618131a7
Vinicius Nakamura <vnakamura@vnakamura.com>
2017-06-29 15:31:26 -0300

    Flow typing for Speak component

--- src/components/Chat/Speak.js
+++ src/components/Chat/Speak.js
@@ -4,12 +4,18 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 
 import type {SpeakMessage} from '../../types/Chat';
+import type {Theme} from '../../theme';
 import Avatar from '../Avatar';
+
+type Props = {
+  byMe: ?boolean,
+  theme: Theme,
+};
 
 const Container = styled.div `
   display: flex;
-  margin: ${props => props.theme.spacing.margin};
-  flex-direction: ${props => props.byMe? 'row-reverse' : 'row'};
+  margin: ${(props: Props) => props.theme.spacing.margin};
+  flex-direction: ${(props: Props) => props.byMe? 'row-reverse' : 'row'};
 `;
 
 const AvatarAtBottom = styled(Avatar) `
@@ -17,20 +23,20 @@ const AvatarAtBottom = styled(Avatar) `
 `;
 
 const Line = styled.div `
-  border-right: ${props => props.byMe? 0 : props.theme.chat.speakBorder};
-  border-left: ${props => props.byMe? props.theme.chat.speakBorder : 0};
-  border-bottom-right-radius: ${props => props.byMe? 0 : props.theme.chat.speakBorderRadius};
-  border-bottom-left-radius: ${props => props.byMe? props.theme.chat.speakBorderRadius : 0};
-  width: ${props => props.theme.chat.speakBorderRadius};
-  margin: 0 ${props => props.byMe? 0 : props.theme.spacing.margin} ${props => props.theme.chat.speakBorderRadius} ${props => props.byMe? props.theme.spacing.margin : 0};
+  border-right: ${(props: Props) => props.byMe? 0 : props.theme.chat.speakBorder};
+  border-left: ${(props: Props) => props.byMe? props.theme.chat.speakBorder : 0};
+  border-bottom-right-radius: ${(props: Props) => props.byMe? 0 : props.theme.chat.speakBorderRadius};
+  border-bottom-left-radius: ${(props: Props) => props.byMe? props.theme.chat.speakBorderRadius : 0};
+  width: ${(props: Props) => props.theme.chat.speakBorderRadius};
+  margin: 0 ${(props: Props) => props.byMe? 0 : props.theme.spacing.margin} ${(props: Props) => props.theme.chat.speakBorderRadius} ${(props: Props) => props.byMe? props.theme.spacing.margin : 0};
 `;
 
 const Text = styled.p `
-  margin: 0 0 ${props => props.theme.chat.speakBorderRadius} 0;
-  padding-bottom: ${props => props.theme.chat.speakBorderRadius};
+  margin: 0 0 ${(props: Props) => props.theme.chat.speakBorderRadius} 0;
+  padding-bottom: ${(props: Props) => props.theme.chat.speakBorderRadius};
   white-space: pre-line;
   flex: 8;
-  text-align: ${props => props.byMe? 'right' : 'left'};
+  text-align: ${(props: Props) => props.byMe? 'right' : 'left'};
 `;
 
 const Margin = styled.div `
